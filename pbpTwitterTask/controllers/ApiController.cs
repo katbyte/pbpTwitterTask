7f6b5d4fbaca754c02d2257007e62de8a8156f46
katelynn <kt@katbyte.me>
2015-09-20 07:11:27 -0700

    added ApiController.cs so /api now redirects to /api/aggregate/{users}

---
+++ pbpTwitterTask/controllers/ApiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Microsoft.AspNet.Mvc;
+
+
+
+namespace katbyte.pbpTwitterTask.controllers {
+
+
+    /// <summary>
+    /// route for /api to redirect to /api/aggregate
+    /// </summary>
+    [Route("api")]
+    public class ApiController : Controller {
+
+        /// <summary>
+        /// route for /api to redirect to /api/aggregate
+        /// </summary>
+        [HttpGet]
+        public ActionResult Get() {
+            return Redirect("/api/aggregate/" + String.Join(";", App.twitterFeed.defaultAccounts));
+        }
+    }
+}
